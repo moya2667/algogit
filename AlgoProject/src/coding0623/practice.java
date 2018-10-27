082a2f8d82c2a74232900391c47ce29c18a814b7
moya <moya2667@gmail.com>
2018-10-27 11:21:14 +0900

    test

--- AlgoProject/src/coding0623/practice.java
+++ AlgoProject/src/coding0623/practice.java
@@ -19,96 +19,100 @@ public class practice {
 	}
 
 	static HashMap map = new HashMap();
+
 	@Test
 	public void test() {
-		
+
 		String a = "aaaa";
-		char[] b = new char[10]; 		
+		char[] b = new char[10];
 		String a1 = new String(b);
-		
+
 		tree tr = new tree();
 		treenode root = new treenode("root".toCharArray());
-		treenode de1 = tr.add(root,"aa".toCharArray());		
-		treenode de2 = tr.add(root,"bb".toCharArray());
-		treenode de1_1 = tr.add(de1,"aaa".toCharArray());
-		treenode de1_1_1 = tr.add(de1_1,"aaaa".toCharArray());
+		treenode de1 = tr.add(root, "aa".toCharArray());
+		treenode de2 = tr.add(root, "bb".toCharArray());
+		treenode de1_1 = tr.add(de1, "aaa".toCharArray());
+		treenode de1_1_1 = tr.add(de1_1, "aaaa".toCharArray());
 		tr.print(root, 0);
-		
+
 		treenode f = tr.find("bb".toCharArray());
 		System.out.println(f.parent.val);
 	}
-	
+
 	class treenode {
 		treenode parent;
-		treenode next , prev;		
-		char[] val ; 
-		llist childlist ; 
-		
-		public treenode (char[] t) { 
-			val = t; //reference
+		treenode next, prev;
+		char[] val;
+		llist childlist;
+
+		public treenode(char[] t) {
+			val = t; // reference
 			childlist = new llist();
 		}
 	}
+
 	class llist {
-		treenode head, tail; 
-		treenode add(treenode pa , char[] d) {
+		treenode head, tail;
+
+		treenode add(treenode pa, char[] d) {
 			String strD = new String(d);
 			treenode ne = new treenode(d);
-			ne.parent = pa ;
-			
-			if ( head == null) { 
+			ne.parent = pa;
+
+			if (head == null) {
 				head = ne;
-				tail = ne;
-				map.put(strD, ne);
-				return ne;
+				tail = ne;								
+			}else {
+				tail.next = ne;
+				ne.prev = tail;
+				tail = ne;	
 			}
-			tail.next = ne ; 
-			ne.prev = tail;
-			tail = ne;
-			
 			map.put(strD, ne);
 			return ne;
 		}
 	}
 
 	class tree {
-		treenode add (treenode cur , char[] d) {
-			return cur.childlist.add(cur , d);
+		treenode add(treenode cur, char[] d) {
+			return cur.childlist.add(cur, d);
 		}
-		
+
 		boolean delete(treenode d) {
 			treenode f = find(d.val);
-			if ( f == null) return false;
-			
+			if (f == null)
+				return false;
+
 			return true;
-			
+
 		}
+
 		treenode find(char[] f) {
 			Object t = map.get(new String(f));
-			return (treenode)t;
+			return (treenode) t;
 		}
-		
-		void print (treenode n , int de ) {
-			if ( n == null) return;
-			
-			for ( int i = 0 ; i < de ; i++) {
+
+		void print(treenode n, int de) {
+			if (n == null)
+				return;
+
+			for (int i = 0; i < de; i++) {
 				System.out.print("+");
 			}
-			System.out.println(n.val);			
-			
+			System.out.println(n.val);
+
 			treenode h = n.childlist.head;
-			
-			if ( h == null) return;
-			
-			print(h , de+1);			
-			
-			while( h != null) { 
+
+			if (h == null)
+				return;
+
+			print(h, de + 1);
+
+			while (h != null) {
 				h = h.next;
-				print(h , de+1);
+				print(h, de + 1);
 			}
-			
+
 		}
 	}
-	
 
 }
