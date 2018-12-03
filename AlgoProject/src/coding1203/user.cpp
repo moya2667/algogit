fbce33681e9d1f8c8395204090cd697acac9a0e8
moya <moya2667@gmail.com>
2018-12-04 00:53:17 +0900

    address user code

--- AlgoProject/src/coding1203/user.cpp
+++ AlgoProject/src/coding1203/user.cpp
@@ -6,8 +6,21 @@
 
 using namespace std;
 #define ASCII 128
-char tree[31] = "";
-int totalCnt = 0;
+char* getCandiBitAgain(char d) {
+	char candi[9] = { '\0' };
+	//System.out.println(d + "에 대한 후보군입니다");
+	int bit[9] = { 0, 1, 2, 4, 8, 16, 32, 64, 128 };
+
+
+	for (int i = 0; i < 9; i++) {
+		//candi[i] = 'a';
+		candi[i] = (char)(d ^ bit[i]);
+		printf("%c", candi[i]);
+	}
+	printf("\n");
+	//printf("%s\n", candi);
+	return candi;
+}
 
 struct trie {
 	bool isFinal = false;
@@ -34,10 +47,11 @@ struct trie {
 			return;
 		}
 
-		int c = str[idx] - 'a';
+		int c = str[idx] ;
 
 		if (node[c] == nullptr) {
 			node[c] = new trie();
+			node[c]->word = str[idx];
 		}
 		node[c]->insert(str, idx + 1, len);
 	}
@@ -51,11 +65,18 @@ struct trie {
 			return false;
 		}
 
-		int c = str[idx] - 'a';
-
-		if (node[c] == NULL) return false;
-
-		return node[c]->find(str, idx + 1, len);
+		char c = str[idx];
+		char* candi = getCandiBitAgain(c);
+		for (int i = 0; i < 9; i++) {
+			int idx = candi[i];
+			if (idx>=0 && idx < 128 && node[idx] != '\0') {
+				printf("1bit 후보군을 찾아 서치시작합니다.\n");
+				printf("1bit 후보군은 : %c 입니다\n" , node[idx]->word );
+				return node[idx]->find(str, idx + 1 , len);
+			}
+			// System.out.println("1bit 후보군 중 맞는 주소록이 없습니다");
+		}
+		return false;
 	}
 
 	int getlength(char* data) {
@@ -67,14 +88,28 @@ struct trie {
 	}
 };
 
+
+struct trie t;
 void getAllowCode(char code[ALLOW_CODE_COUNT + 1]) {
 
 }
 
 void getCode(int N, char code[MAX_CODE_SIZE][CODE_LENGTH + 1]) {
 
+	for (int i = 0; i < N; i++) {
+		t.insert(code[i], 0, CODE_LENGTH+1);		
+	}
+
+	/*
+	bool find = t.find(code[0], 0, CODE_LENGTH + 1);
+	printf("%d\n", find);
+	*/
 }
 
 void restore(char errorCode[CODE_LENGTH + 1], char prediction[CODE_LENGTH + 1]) {
+
+	printf(" %d \n" , t.find(errorCode, 0, CODE_LENGTH + 1));
+
+	printf("1");
 
 }
