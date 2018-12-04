7842018aba676b8c8dd31075454911c92d82d960
moya <moya2667@gmail.com>
2018-12-04 23:54:27 +0900

    update address.java

--- AlgoProject/src/coding0331/address.java
+++ AlgoProject/src/coding0331/address.java
@@ -4,100 +4,41 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class address {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	tri t = new tri();
-	static int total = 0;
-	static int bitdiff = 0;
+	static tri t = new tri();
+	static char[] fData = new char[6];
 
 	@Test
 	public void test() throws FileNotFoundException {
-        FileInputStream ip = new FileInputStream("src\\coding0331\\sample.txt");     
-        Scanner sc = new Scanner(ip);
-        int c = sc.nextInt();
-        
-        for (int i=0 ; i< c ;i++) {
-            char[] data = sc.next().toCharArray();
-            System.out.println(data);
-            t.insert(data, 0);
-        }
-        
-        System.out.println(t.isfind("d.CL)^".toCharArray(), 0));
-	}
+		FileInputStream ip = new FileInputStream("src\\coding0331\\sample_input.txt");
+		Scanner sc = new Scanner(ip);
+		int c = sc.nextInt();
 
-	/*
-	 * @Test public void test1() { //t.insert("hdll".toCharArray(),0);
-	 * t.insert("mell".toCharArray(),0);
-	 * System.out.println(t.isfind("gcll".toCharArray(),0)); System.out.println(
-	 * "bit diff = " + bitdiff);
-	 * 
-	 * st.size(); for ( int i = 0 ; i < st.size(); i++) {
-	 * System.out.print((char)st.get(i)); } System.out.println();
-	 * 
-	 * 
-	 * st.clear(); bitdiff = 0;
-	 * System.out.println(t.isfind("hehl".toCharArray(),0)); System.out.println(
-	 * "bit diff = " + bitdiff);
-	 * 
-	 * for ( int i = 0 ; i < st.size(); i++) {
-	 * System.out.print((char)st.get(i)); } System.out.println(); }
-	 */
-
-	@Test
-	public void getCandi1Bit() {
-		char value = 'a';
-		System.out.println((int) value);
-		int[] bit = new int[] { 1, 2, 4, 8, 16, 32, 64, 128 };
-
-		for (int i = 0; i < bit.length; i++) {
-			int v = value ^ bit[i];
-			System.out.print(v + ".");
+		for (int i = 0; i < c; i++) {
+			char[] data = sc.next().toCharArray();
+			System.out.println(data);
+			t.insert(data, 0);
 		}
-		System.out.println();
 
+		// 0 ow:I;4
+		while (true) {
+			if (sc.nextInt() == -1) {
+				break;
+			}
+			char[] data = sc.next().toCharArray();
+			if (t.isfind(data, 0)) {
+				System.out.println("존재합니다 : " + new String(fData));
+				fData = new char[6];
+			} else {
+				System.out.println("존재하지않습니다");
+			}
+		}
 	}
 
-	@Test
-	public void test2() {
-		// t.insert("fell".toCharArray(),0);
-		t.insert("owzH+t".toCharArray(), 0);
-		t.insert("ipll".toCharArray(), 0);
-
-		// System.out.println(t.isfind("fell".toCharArray(),0));
-		// System.out.println(t.isfind("cccc".toCharArray(),0));
-		System.out.println(t.isfind("owzH+t".toCharArray(), 0));
-		System.out.println(t.isfind("ipll".toCharArray(), 0));
-		// System.out.println("bit diff = " + bitdiff);
-
-		/*
-		 * for (int i = 0; i < 9; i++) { System.out.print(bit[i]); }
-		 */
-
-		System.out.println(decisionStr);
-		/*
-		 * st.size(); for ( int i = 0 ; i < st.size(); i++) {
-		 * System.out.print((char)st.get(i)); } System.out.println();
-		 * d.CL)^
-		 */
-	}
-
-	static int bitC = 0;
-	static int decisionbit = 999;
-	static String decisionStr = "";
-
-	class tri {
+	static class tri {
 
 		boolean isfinish = false;
 		boolean isnext = false;
@@ -109,7 +50,6 @@ public class address {
 		tri[] t = new tri[128];
 
 		tri() {
-
 		}
 
 		public void insert(char[] chars, int start) {
@@ -128,12 +68,6 @@ public class address {
 				t[idx].c = chars[start];
 			}
 
-			/*
-			 * int c = 0; for (int i = 0 ; i < 28 ; i++) { if ( t[i] != null ) {
-			 * //System.out.print(t[i].c + " : "); c++; } }
-			 */
-
-			// System.out.println(c);
 			if (c > 1)
 				isChilds = true;
 
@@ -143,26 +77,23 @@ public class address {
 		public boolean isfind(char[] chars, int start) {
 			// 끝까지 같는데, 진짜 끝이라면
 			if (chars.length == start && isfinish == true) {
-				/*
-				 * if (decisionbit > bitdiff) { decisionStr = "";
-				 * 
-				 * decisionbit = bitdiff; bitdiff=0; }
-				 */
 				return true;
 			} else if (chars.length == start && isfinish == false) {
 				return false;
 			}
 
-			int originIdx = chars[start];
-
 			char[] candi = getCandiBitAgain(chars[start]);
 			for (int i = 0; i < 9; i++) {
-				int idx = candi[i];				
+				int idx = candi[i];
 				if (idx < 128 && t[idx] != null) {
-					System.out.println("1bit 후보군을 찾아 서치시작합니다.");
-					System.out.println("1bit 후보군은 : " + t[idx].c + ":" + (int) t[idx].c + " 입니다");
-					return t[idx].isfind(chars, start + 1);
-
+					// System.out.println("1bit 후보군을 찾아 서치시작합니다.");
+					// System.out.println("1bit 후보군은 : " + t[idx].c + ":" +
+					// (int) t[idx].c + " 입니다");
+					if (t[idx].isfind(chars, start + 1)) {
+						// System.out.println(t[idx].c);
+						fData[start] = t[idx].c; // 재귀 리콜을 통한 역순 자동저장
+						return true;
+					}
 				}
 				// System.out.println("1bit 후보군 중 맞는 주소록이 없습니다");
 			}
@@ -171,7 +102,7 @@ public class address {
 
 		private char[] getCandiBitAgain(char d) {
 			char[] candi = new char[9];
-			System.out.println(d + "에 대한 후보군입니다");
+			// System.out.println(d + "에 대한 후보군입니다");
 
 			int[] bit = new int[] { 0, 1, 2, 4, 8, 16, 32, 64, 128 };
 
@@ -179,27 +110,24 @@ public class address {
 				candi[i] = (char) (d ^ bit[i]);
 			}
 
-			for (int i = 0; i < bit.length; i++) {
-				System.out.print(candi[i] + ":" + (int) candi[i] +" ");
-			}
-			System.out.println();
+			// for (int i = 0; i < bit.length; i++) {
+			// System.out.print(candi[i] + ":" + (int) candi[i] +" ");
+			// }
+			// System.out.println();
 			return candi;
 		}
-		
-		private char[] getCandiBit(char d) {
-			int a = 1;
-			char[] candi = new char[9];
-			int cnt = 1;
+	}
 
-			candi[0] = d;
-			for (int i = 0; i < 9; i++) {
-				int v = (d + (a << i));
-				System.out.print(v + " ");
-				if (v > 129)
-					break;
-				candi[cnt++] = (char) v;
-			}
-			return candi;
+	@Test
+	public void getCandi1Bit() {
+		char value = 'a';
+		System.out.println((int) value);
+		int[] bit = new int[] { 1, 2, 4, 8, 16, 32, 64, 128 };
+
+		for (int i = 0; i < bit.length; i++) {
+			int v = value ^ bit[i];
+			System.out.print(v + ".");
 		}
+		System.out.println();
 	}
 }
