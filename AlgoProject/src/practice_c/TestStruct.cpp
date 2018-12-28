cd78ac1d6b329941fcb7d5940fb326247e0144c9
moya <moya2667@gmail.com>
2018-12-29 01:13:14 +0900

    Update Struct

--- AlgoProject/src/practice_c/TestStruct.cpp
+++ AlgoProject/src/practice_c/TestStruct.cpp
@@ -1,4 +1,4 @@
-癤�#include <stdio.h>
+#include <stdio.h>
 using namespace std;
 
 struct MyStruct
@@ -8,18 +8,28 @@ struct MyStruct
 };
 
 void print(MyStruct* pointer) {
-	pointer->data = "aaa";
+	pointer->data = "void print";
 	pointer->a = 10;
 }
 
+
+//참고 | 구조체 변수의 메모리 주소 반환?
+//함수가 끝나면 구조체 변수도 사라집니다.따라서 & (주소 연산자)로 구조체 변수의 메모리 주소를 반환하면 안 됩니다.
+//https://dojang.io/mod/page/view.php?id=531
 MyStruct* print(MyStruct pointer) {
-	pointer.data = "aaa";
+	pointer.data = "MyStruct* print";
 	pointer.a = 10;
 	return &pointer;
 }
 
+MyStruct* printTT(MyStruct *pointer) {
+	pointer->data = "MyStruct* print";
+	pointer->a = 10;
+	return pointer;
+}
+
 MyStruct print1(MyStruct pointer) {
-	pointer.data = "aaa";
+	pointer.data = "print1";
 	pointer.a = 10;
 	return pointer;
 }
@@ -31,13 +41,15 @@ int main() {
 	print(myPointer);
 	printf("%s %d\n", myPointer->data, myPointer->a);
 
-	MyStruct* test = print(my);	
-	printf("%s %d\n", test->data, test->a);
-
 	MyStruct test1 = print1(my);
 	printf("%s %d\n", test1.data, test1.a);
+
+	MyStruct* test = printTT(myPointer);
+	printf("%s %d\n", test->data, test->a);
+	
+	//MyStruct* test = print(my);
+	//printf("%s %d\n", test->data, test->a);	
+
 	return 0;
 }
-
-
 
