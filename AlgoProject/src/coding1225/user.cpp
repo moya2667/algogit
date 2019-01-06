5ead372096e59ff360ef3d6097ac91e8c3276539
moya <moya2667@gmail.com>
2019-01-07 01:47:55 +0900

    update

--- AlgoProject/src/coding1225/user.cpp
+++ AlgoProject/src/coding1225/user.cpp
@@ -17,47 +17,57 @@ struct Club {
 	int playerCnt;
 };
 
+void print();
+
 Club clubs[MAX_CLUB+1];
 Player players[10000];
 
 #define DEBUG 1
 
-int LOCATION_CAPA; //지역크기
-int CLUB_NUMBER; //클럽수 
-int CLUB_PLAYER_NUMBER; //클럽당 선수 수
-int CLUB_LOCATION[MAX_CLUB+1];
+static int LOCATION_CAPA; //지역크기
+static int CLUB_NUMBER; //클럽수 
+static int CLUB_PLAYER_NUMBER; //클럽당 선수 수
+static int CLUB_LOCATION[MAX_CLUB+1];
 
 int getMinLocationTeam(Player player, int serchStep) {
 	int minDiff = 9999;
-	int minClubIDs[MAX_CLUB + 1] = { 0 }; //이렇게 해야하나.
+	int minClubId = 9999;
 	//주변 club 위치를 파악 한다 
 	int clubCnt = 0;
-	for (int i = 1; i <= CLUB_NUMBER; i++) {
-		int diff = clubs[i].location - player.location;
-		if (diff < 0) diff = diff*(-1);
+	for (int i = 0; i < CLUB_NUMBER; i++) {
+		//하 씨..두가지 case 존재 (이런걸 조심해야하는데.)
+		int a = LOCATION_CAPA - clubs[i].location - player.location;
+		if (a < 0) a = a*-1;		
+		int b = clubs[i].location - player.location;
+		if (b < 0) b = b*-1;
 
-		if (diff <= minDiff) {
+		int diff = 9999;
+		if (a < b) diff = a; 
+		else {
+			diff = b;
+		}		
+
+		//diff 가 더 최소값이라면, 
+		if (diff < minDiff) {
 			minDiff = diff ;
-			minClubIDs[clubCnt++] = i;
+			minClubId = i;
+		}
+		//같을경우 이미 club id 가 더 작은것이 들어가있는거이기 때문에 무시해도 된다.
+		else if (diff == minDiff) {
+			printf("같은 거리 팀 ID 발견 : minclud id = %d , same distance clud id = %d \n", minClubId , i); 
 		}
 	}
-
-	//club 위치가 동일할 경우, cid 가 적은 club 을 선정 하여, 결과값을 준다
-	int clubID = 9999;
-	int cnt = 0;
-	while (clubCnt > 0) {
-		if (clubID > minClubIDs[cnt]) {
-			clubID = minClubIDs[cnt];			
-		}
-		cnt++;
-		clubCnt--; //코드 마음에 안드네.
-	}	
 
 	//9999가 나올수 없다 
-	if ( DEBUG && clubID == 9999) {
-		printf("Clud ID가 9999 어떻게 된일\n");
+	if (DEBUG) { 
+		if ( minClubId == 9999) {
+			printf("Clud ID가 9999 어떻게 된일\n");
+		}
+		else {
+			printf("최종선택된 Club ID : %d\n", minClubId);
+		}
 	}
- 	return clubID;
+ 	return minClubId;
 }
 
 void Init(int L, int N, int C, int location[MAX_CLUB]) {
@@ -65,13 +75,20 @@ void Init(int L, int N, int C, int location[MAX_CLUB]) {
 	LOCATION_CAPA = L;
 	CLUB_NUMBER = N;
 	CLUB_PLAYER_NUMBER = C;
-	int cnt = 1; 
+	
 	for (int i = 0; i < N; i++) {
-		clubs[cnt].location = location[i];
-		clubs[cnt].clubID = i;
-		clubs[cnt].head = nullptr;
-		clubs[cnt].tail = nullptr;
-		cnt++;
+		clubs[i].location = location[i];
+		clubs[i].clubID = i;
+		clubs[i].head = nullptr;
+		clubs[i].tail = nullptr;		
+	}
+
+	print();
+}
+
+void print() {
+	for (int i = 0; i < CLUB_NUMBER; i++) {
+		printf("club id= %d location = %d cnt = %d\n", clubs[i].clubID, clubs[i].location, clubs[i].playerCnt);
 	}
 }
 
@@ -91,6 +108,7 @@ void sort(Club club) {
 }
 
 int AddPlayer(int pid, int location, int power) {
+	if (DEBUG) printf("[%s] pid(%d) location(%d) power(%d)\n", __FUNCTION__, pid, location, power);
 	players[pid].location = location;
 	players[pid].power = power;
 	int searchStep =  0;
