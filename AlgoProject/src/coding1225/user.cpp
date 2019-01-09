7c67af5cb010cf3329509d1358b4189c9abbd28e
moya <moya2667@gmail.com>
2019-01-10 00:49:31 +0900

    difficult

--- AlgoProject/src/coding1225/user.cpp
+++ AlgoProject/src/coding1225/user.cpp
@@ -76,7 +76,7 @@ void Init(int L, int N, int C, int location[MAX_CLUB]) {
 	//지역크기 
 	LOCATION_CAPA = L;
 	CLUB_NUMBER = N;
-	CLUB_PLAYER_NUMBER = 3;
+	CLUB_PLAYER_NUMBER = C;
 
 	for (int i = 0; i < N; i++) {
 		clubs[i].location = location[i];
@@ -103,6 +103,10 @@ void print(int cid) {
 	}
 }
 
+void print(Player* player) {	
+	printf("player id: %d priority : %d location : %d power= %d \n", player->pid, player->location,player->priority , player->power);
+}
+
 bool isFull(Club club) {
 	if (club.playerCnt != CLUB_PLAYER_NUMBER) {
 		return false;
@@ -110,41 +114,46 @@ bool isFull(Club club) {
 	return true;
 }
 
-Player changeClubPlayer(Club club, Player player) {
+Player* changeClubPlayer(Club club, Player* player) {
+	//print(player);
+	Player* p = club.head;
+	
+	while (p != nullptr) {
+		//player가 더 크다면 ,넣어야 함
+		if (p->priority < player->priority) {
+			Player* pe = p->prev;
+			Player* ne = p->next;
+			//이전께 null 이 아니라면 pe 와 연결 
+			if (pe != nullptr) {
+				pe->next = player; 
+				player->prev = pe;				
+			}
+			if (ne != nullptr) {
+				ne->prev = player;
+				player->next = ne;
+			}
+			//print(p);
+			return p;
+		}
+		p = p->next;
+	}
+
 	return player;
 }
 
-void sort(Club club) {
-
-}
 
 //prioirty 가 가장 높은게 맨 앞에 있도록 함
-void addClubToPlayer(int cid, Player* player) {	
-	int clubPlayerCount= clubs[cid].playerCnt;
+void addClubToPlayer(int cid, Player* player) {
+	int clubPlayerCount = clubs[cid].playerCnt;
 	clubs[cid].playerCnt++; //clud안에 player 등록 수 증가
 
 	Player* p = clubs[cid].head;
 	//아무것도 없는 상태라면
-	if (clubPlayerCount == 0) {
+	if (clubs[cid].head == nullptr) {
 		clubs[cid].head = player;
 		clubs[cid].tail = player;
 		return;
-	}
-	//1개 일경우
-	else if (clubPlayerCount == 1) {
-		//head 라면 head 앞에	
-		if (p->priority < player->priority) {
-			player->next = clubs[cid].head;
-			clubs[cid].head->prev = player;
-			clubs[cid].head = player;			
-		}
-		else {
-			clubs[cid].tail->next = player;
-			player->prev = clubs[cid].tail;
-			clubs[cid].tail = player;
-		}
-		return;
-	}
+	}	
 
 	//2개 이상일경우 차례대로 우선순위를 검사하여, 끼어 넣는다.	
 	while (p != nullptr) {
@@ -154,51 +163,25 @@ void addClubToPlayer(int cid, Player* player) {
 			if (p == clubs[cid].head) {
 				player->next = clubs[cid].head;
 				clubs[cid].head->prev = player;
-				clubs[cid].head = player;
-				return;
+				clubs[cid].head = player;				
 			}
-			else if (p == clubs[cid].tail) {
-				Player* pe = p->prev;
-				pe->next = player; 
-				player->prev = pe; 
-				player->next = clubs[cid].tail;
-				clubs[cid].tail->prev = player;				
-				return;
-			}
-			//중간에
-			Player* pe = p->prev;
-			Player* ne = p->next;
-
-			pe->next = player;
-			player->prev = pe;
-			player->next = ne;
-			ne->prev = player;
+			//head 아닐경우 아래 처리 
+			else {
+				Player* pre = p->prev;
+				pre->next = player;
+				player->prev = pre;
+				player->next = p;								
+			}			
 			return;
-		}		
+		}
 		p = p->next;
 	}
-		
+
 	//마지막까지 같을경우 (최하위 우선순위라면)
 	clubs[cid].tail->next = player;
 	player->prev = clubs[cid].tail;
 	clubs[cid].tail = player;
 	return;
-}
-
-void addClubToPlayerTemp(int cid, Player* player) {
-	clubs[cid].playerCnt++; //clud안에 player 등록 수 증가	
-
-	//그냥 넣는게 아니네.. prioirty 근간으로 넣어야지.
-	if (clubs[cid].head == nullptr) {
-		clubs[cid].head = player;
-		clubs[cid].tail = player;
-	}
-	else {
-		clubs[cid].tail->next = player;
-		player->prev = clubs[cid].tail;
-		clubs[cid].tail = player;
-		player->next = nullptr;
-	}
 }
 
 int AddPlayer(int pid, int location, int power) {
@@ -226,7 +209,9 @@ int AddPlayer(int pid, int location, int power) {
 
 		//해당 Club 소속 선수의 능력치가 낮을경우 신규소속 선수 넣는다, 
 		//능력치가 동일할 경우 , pid가 더 낮은 선수를 넣는다	
-		Player player = changeClubPlayer(clubs[cid], players[pid]);
+		print(&players[pid]);
+		Player* player = changeClubPlayer(clubs[cid], &players[pid]);
+		print(player);
 
 		//기존 선수를 뺐거나, 들어갈수 없다면, 그 다음 club 가 가장 가까운 지역에 있는 club id를 찾는다. 
 		searchStep++;
@@ -254,4 +239,72 @@ int RemovePlayer(int pid) {
 
 int GetPlayerCount(int cid) {
 	return clubs[cid].playerCnt;
+}
+
+
+//prioirty 가 가장 높은게 맨 앞에 있도록 함
+void addClubToPlayerTemp(int cid, Player* player) {
+	int clubPlayerCount = clubs[cid].playerCnt;
+	clubs[cid].playerCnt++; //clud안에 player 등록 수 증가
+
+	Player* p = clubs[cid].head;
+	//아무것도 없는 상태라면
+	if (clubPlayerCount == 0) {
+		clubs[cid].head = player;
+		clubs[cid].tail = player;
+		return;
+	}
+	//1개 일경우
+	else if (clubPlayerCount == 1) {
+		//head 라면 head 앞에	
+		if (p->priority < player->priority) {
+			player->next = clubs[cid].head;
+			clubs[cid].head->prev = player;
+			clubs[cid].head = player;
+		}
+		else {
+			clubs[cid].tail->next = player;
+			player->prev = clubs[cid].tail;
+			clubs[cid].tail = player;
+		}
+		return;
+	}
+
+	//2개 이상일경우 차례대로 우선순위를 검사하여, 끼어 넣는다.	
+	while (p != nullptr) {
+		//player가 더 크다면 ,넣어야 함
+		if (p->priority < player->priority) {
+			//head 라면 head 앞에 			
+			if (p == clubs[cid].head) {
+				player->next = clubs[cid].head;
+				clubs[cid].head->prev = player;
+				clubs[cid].head = player;
+				return;
+			}
+			else if (p == clubs[cid].tail) {
+				Player* pe = p->prev;
+				pe->next = player;
+				player->prev = pe;
+				player->next = clubs[cid].tail;
+				clubs[cid].tail->prev = player;
+				return;
+			}
+			//중간에
+			Player* pe = p->prev;
+			Player* ne = p->next;
+
+			pe->next = player;
+			player->prev = pe;
+			player->next = ne;
+			ne->prev = player;
+			return;
+		}
+		p = p->next;
+	}
+
+	//마지막까지 같을경우 (최하위 우선순위라면)
+	clubs[cid].tail->next = player;
+	player->prev = clubs[cid].tail;
+	clubs[cid].tail = player;
+	return;
 }
