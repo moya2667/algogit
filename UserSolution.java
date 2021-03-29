55cdaa4a9ae110f90037e6ee5d092027f21f457a
모야 <moya2667@gmail.com>
2021-03-30 02:49:08 +0900

    Update UserSolution.java

--- UserSolution.java
+++ UserSolution.java
@@ -1,84 +1,184 @@
-class UserSolution {
-	
-	int MAX = 100002;
-	Hashtable myhash;
-	pq mypq;
+class UserSolution2 {
+
+	/// Constants
+	final static int MAX_VALUE_LEN = 100001;
+
+	/// Variables
 	node[] nodes;
-	int cnt;
-	
-	public void init() {
-		myhash = new Hashtable(200000);
-		mypq = new pq();
-		
-		nodes = new node[100002];
-		cnt  = 0 ;
-	}  
-	
-	class node{
-		int key ;
-		int value ;
-		boolean delete = false;
-		node(){};
-	}
-	
-	private int createnode(int k , int v) { 
-		if (nodes[cnt] == null) { 
-			nodes[cnt] = new node();
+	Hashtable ht;
+	PQ pq;
+	int count;
+	node[] popped = new node[10];
+	/// Methods
+
+	class node {
+		int value;
+		int pqIndex;
+
+		public node(int value) {
+			this.value = value;
+			this.pqIndex = -1;
 		}
-		nodes[cnt].key = k ; 
-		nodes[cnt].value = v;
-		return cnt++;
+	}
+
+	public void init() {
+		nodes = new node[MAX_VALUE_LEN];
+		ht = new Hashtable(MAX_VALUE_LEN + 6);
+		pq = new PQ();
+		pq.heapSize = 0;
+		count = 0;
 	}
 
 	public void push(int key, int value) {
-		int idx = createnode(key,value);
-		myhash.add(key, idx);
-		mypq.heapPush(idx);
+		int index = ht.find(key);
+		if (index == -1) {
+			nodes[count] = new node(value);
+			ht.add(key, count);
+			pq.push(nodes[count]);
+			count++;
+		} else {
+			nodes[index] = new node(value);
+			pq.push(nodes[index]);
+		}
+
 	}
 
 	public int top(int K) {
-		int c = 1;
-		int sum = 0; 
-		while(c <= K) {
-			int idx = mypq.heapPop();
-			if (nodes[idx].delete == false) { 
-				sum+=nodes[idx].value;
-				c++; 
-			}
+		int ret = 0;
+
+		for (int i = 0; i < K; i++) {
+			node value = pq.pop();
+			popped[i] = value;
+			ret += value.value;
 		}
- 		return sum;
+
+		for (int i = 0; i < K; i++) {
+			pq.push(popped[i]);
+		}
+		return ret;
 	}
 
 	public void erase(int key) {
-		System.out.println("erase");
-		int idx = myhash.find(key);
-		nodes[idx].delete = true;
-		myhash.delete(key);
+		int index = ht.find(key);
+		pq.popByIndex(nodes[index].pqIndex);
 	}
 
-	public void modify(int key, int value) {		
-		System.out.println("modify: " + key + " , " + value);
-		int idx = myhash.find(key);
-		nodes[idx].value = value;
-		
-		int[] check  = new int[mypq.heapSize];
-		int c = 0;
-		while(true){
-			int popIdx = mypq.heapPop();
-			check[c++] = popIdx;
-			if ( popIdx == idx ) {
-				for (int i = 0; i < c ; i++) {
-					mypq.heapPush(check[i]);
-				}
-				break;
+	public void modify(int key, int value) {
+		int index = ht.find(key);
+		nodes[index].value = value;
+		pq.update(nodes[index].pqIndex);
+	}
+
+	class PQ {
+		node heap[];
+		int heapSize = 0;
+
+		public PQ() {
+			heapSize = 0;
+			heap = new node[MAX_VALUE_LEN];
+		}
+
+		boolean canPushSwap(int current) {
+			if (heap[current].value > heap[(current - 1) / 2].value)
+				return true;
+			return false;
+		}
+
+		void upgrade(int current) {
+			while (current > 0 && canPushSwap(current)) {
+				node temp = heap[(current - 1) / 2];
+				heap[(current - 1) / 2] = heap[current];
+				heap[(current - 1) / 2].pqIndex = (current - 1) / 2;
+				heap[current] = temp;
+				heap[current].pqIndex = current;
+				current = (current - 1) / 2;
 			}
+		}
+
+		void push(node value) {
+			if (heapSize + 1 > MAX_VALUE_LEN) {
+				return;
+			}
+
+			heap[heapSize] = value;
+			heap[heapSize].pqIndex = heapSize;
+
+			int current = heapSize;
+			upgrade(current);
+			heapSize = heapSize + 1;
+		}
+
+		boolean canPopSwap(int current) {
+			if (heap[current * 2 + 1].value > heap[current * 2 + 2].value)
+				return true;
+			return false;
+		}
+
+		void downgrade(int current) {
+			while (current < heapSize && current * 2 + 1 < heapSize) {
+				int child;
+				if (current * 2 + 2 >= heapSize) {
+					child = current * 2 + 1;
+				} else {
+					child = canPopSwap(current) ? current * 2 + 1 : current * 2 + 2;
+				}
+
+				if (heap[current].value > heap[child].value) {
+					break;
+				}
+
+				node temp = heap[current];
+				heap[current] = heap[child];
+				heap[current].pqIndex = current;
+				heap[child] = temp;
+				heap[child].pqIndex = child;
+				current = child;
+			}
+		}
+
+		node pop() {
+			if (heapSize <= 0) {
+				return null;
+			}
+
+			node value = heap[0];
+			heapSize = heapSize - 1;
+
+			heap[0] = heap[heapSize];
+			heap[0].pqIndex = 0;
+
+			downgrade(0);
+
+			return value;
+		}
+
+		node popByIndex(int index) {
+			if (heapSize <= 0) {
+				return null;
+			}
+
+			node value = heap[index];
+			heapSize = heapSize - 1;
+
+			heap[index] = heap[heapSize];
+			heap[index].pqIndex = index;
+
+			upgrade(index);
+			downgrade(index);
+
+			return value;
+		}
+
+		void update(int index) {
+			upgrade(index);
+			downgrade(index);
 		}
 	}
 
 	class Hashtable {
 		class Hash {
 			int key;
-			int value;
+			int data;
 		}
 
 		int capacity;
@@ -92,8 +192,12 @@ class UserSolution {
 			}
 		}
 
-		private int hash(int key) {
-			return key % capacity;
+		private int hash(int k) {
+			int hash = 5381;
+			hash = ((hash << 5) + hash) + k;
+			if (hash < 0)
+				hash *= -1;
+			return hash % capacity;
 		}
 
 		public int find(int key) {
@@ -101,113 +205,24 @@ class UserSolution {
 			int cnt = capacity;
 			while (tb[h].key != 0 && (--cnt) != 0) {
 				if (tb[h].key == key) {
-					return tb[h].value;
+					return tb[h].data;
 				}
 				h = (h + 1) % capacity;
 			}
-			return 0;
+			return -1;
 		}
 
-		public int delete(int key) {
-			int h = hash(key);
-			int cnt = capacity;
-			while (tb[h].key != 0 && (--cnt) != 0) {
-				if (tb[h].key == key) {
-					int v = tb[h].value;
-					tb[h] = new Hash();
-					return v;
-				}
-				h = (h + 1) % capacity;
-			}
-			return 0;
-		}
-
-		int add(int key, int value) {
+		void add(int key, int data) {
 			int h = hash(key);
 			while (tb[h].key != 0) {
 				if (tb[h].key == key) {
-					return value;
+					return;
 				}
 				h = (h + 1) % capacity;
 			}
 			tb[h].key = key;
-			tb[h].value = value;
-			return value;
-		}
-
-	}
-	
-	class pq {
-		int heap[] = new int[MAX];
-		int heapSize = 0;
-
-		void heapInit() {
-			heapSize = 0;
-		}
-
-		void heapPush(int value) {
-			if (heapSize + 1 > MAX) {
-				return;
-			}
-
-			heap[heapSize] = value;
-
-			int current = heapSize;
-			while (current > 0 && iscompare(heap[current] , heap[(current - 1) / 2])) {
-				int temp = heap[(current - 1) / 2];
-				heap[(current - 1) / 2] = heap[current];
-				heap[current] = temp;
-				current = (current - 1) / 2;
-			}
-
-			heapSize = heapSize + 1;
-		}
-
-		int heapPop() {
-			if (heapSize <= 0) {
-				return -1;
-			}
-
-			int value = heap[0];
-			heapSize = heapSize - 1;
-
-			heap[0] = heap[heapSize];
-
-			int current = 0;
-			while (current < heapSize && current * 2 + 1 < heapSize) {
-				int child;
-				if (current * 2 + 2 >= heapSize) {
-					child = current * 2 + 1;
-				} else {
-					child = iscompare(heap[current * 2 + 1] , heap[current * 2 + 2]) ? current * 2 + 1 : current * 2 + 2;
-				}
-
-				if (iscompare(heap[current] , heap[child])) {
-					break;
-				}
-
-				int temp = heap[current];
-				heap[current] = heap[child];
-				heap[child] = temp;
-
-				current = child;
-			}
-			return value;
-		}
-		
-		boolean iscompare(int id, int id2) { 
-			if (nodes[id].value > nodes[id2].value) { 
-				return true;
-			}
-			return false;
-		}
-
-		void heapPrint(int[] heap, int heap_size) {
-			for (int i = 0; i < heap_size; i++) {
-				System.out.print(heap[i] + " ");
-			}
-			System.out.println();
+			tb[h].data = data;
+			return;
 		}
 	}
-	
 }
